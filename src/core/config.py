5e03fa52dccdda357eda26e835a2d8520e364247
Gealor <gzazulya@gmail.com>
2025-03-22 22:27:00 +0400

    add nameing convention for models

--- src/core/config.py
+++ src/core/config.py
@@ -21,13 +21,21 @@ class DatabaseConfig(BaseModel):
     pool_size: int
     max_overflow: int
 
+    naming_convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s"
+    }
+
     def get_db_url(self):
         return f"postgresql+asyncpg://{self.user}:{self.password}@{self.host}:{self.port}/{self.name}"
         
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file = ("src/.env.template", "src/.env"),
+        env_file = (".env.template", ".env"),
         case_sensitive = False,
         env_nested_delimiter="__",
         env_prefix = "APP_CONFIG__",
