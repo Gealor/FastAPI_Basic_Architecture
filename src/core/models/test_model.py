4bb3c0204bec16a2e06590bae5a903502987fd78
Gealor <gzazulya@gmail.com>
2025-03-22 22:28:09 +0400

    add naming_convetion in Base, some test field in test model

--- src/core/models/test_model.py
+++ src/core/models/test_model.py
@@ -1,4 +1,5 @@
 from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import UniqueConstraint
 
 from .base import Base
 
@@ -6,3 +7,11 @@ class User(Base):
     __tablename__ = "users"
 
     username : Mapped[str] = mapped_column(unique = True)
+    mail : Mapped[str] = mapped_column(unique = True)
+    foo : Mapped[int]
+    baz : Mapped[int]
+
+    # __table_args__ = (
+    #     # foo и baz образовывают в совокупности уникальную комбинацию для каждой строки
+    #     UniqueConstraint("foo", "baz"),
+    # )
