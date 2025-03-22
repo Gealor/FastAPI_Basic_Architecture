17f68f565cc2a9a0b388ed99d19dcdf22a629209
Gealor <gzazulya@gmail.com>
2025-03-22 20:14:19 +0400

    config read from .env, add .env.template, add db_helper.dispose to main app

--- src/core/config.py
+++ src/core/config.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, PostgresDsn
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 # конфиги для запуска приложения
 class RunConfig(BaseModel):
@@ -14,17 +14,25 @@ class DatabaseConfig(BaseModel):
     password: str
     host: str
     port: str
-    db_name: str
+    name: str
 
-    echo : bool = False
-    echo_pool:  bool = False
-    pool_size: int = 50
-    max_overflow: int = 10
+    echo : bool
+    echo_pool:  bool 
+    pool_size: int
+    max_overflow: int
 
-    def get_db_url() -> PostgresDsn:
-        url = f""
+    def get_db_url(self):
+        return f"postgresql+asyncpg://{self.user}:{self.password}@{self.host}:{self.port}/{self.name}"
+        
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file = ("src/.env.template", "src/.env"),
+        case_sensitive = False,
+        env_nested_delimiter="__",
+        env_prefix = "APP_CONFIG__",
+    )
+
     run: RunConfig = RunConfig()
     api: ApiPrefixConfig = ApiPrefixConfig()
     db: DatabaseConfig
