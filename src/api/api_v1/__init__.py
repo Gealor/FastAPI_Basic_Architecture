373f60f57a13bc590754ab8be6fcd2cb8ab8d671
Gealor <gzazulya@gmail.com>
2025-04-17 14:27:19 +0400

    init for apiV1

---
+++ src/api/api_v1/__init__.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter
+
+from core.config import settings
+from .users import router as users_router
+from .info import router as info_router
+
+router = APIRouter(
+    prefix = settings.api.v1.prefix,
+)
+router.include_router(
+    users_router,
+    prefix = settings.api.v1.users,
+)
+router.include_router(
+    info_router,
+    prefix = settings.api.v1.info,
+)
