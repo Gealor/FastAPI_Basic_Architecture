2da8251dac681486bafcf64b50d6f41437f6dad9
Gealor <gzazulya@gmail.com>
2025-04-17 14:21:42 +0400

    combined information and users schemas

---
+++ src/core/schemas/info_and_user.py
@@ -0,0 +1,11 @@
+from typing import Optional
+from core.schemas.info import InfoBase, InfoRead
+from core.schemas.user import UserBase, UserRead
+
+
+class UserWithInfo(UserRead):
+    info_user: Optional[InfoBase] = None # для того чтобы не было ошибки, если в базе данных нет информации о пользователе
+    # объявляем так чтобы fastapi видел что эти данные подтягиваются из другой таблицы с помощью join и выдавал данные из этой таблицы
+
+class InfoWithUser(InfoRead):
+    user : Optional[UserBase] = None
