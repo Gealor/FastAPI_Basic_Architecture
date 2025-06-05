8c25410fa0dadacad7e685f69540753d345d3a9b
Gealor <gzazulya@gmail.com>
2025-06-05 22:45:21 +0400

    main router for auth api

---
+++ src/api/auth_api/__init__.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter
+from core.config import settings
+from .basic_auth import router as auth_router
+from .header_auth import router as header_auth_router
+from .cookie_auth import router as cookie_auth_router
+
+router = APIRouter(
+    prefix = settings.api.auth.prefix,
+    tags = ["Auth"],
+)
+
+router.include_router(
+    auth_router,
+    prefix = settings.api.auth.basic_auth,
+)
+
+router.include_router(
+    header_auth_router,
+    prefix = settings.api.auth.header_auth,
+)
+
+router.include_router(
+    cookie_auth_router,
+    prefix = settings.api.auth.cookie_auth,
+)
+
