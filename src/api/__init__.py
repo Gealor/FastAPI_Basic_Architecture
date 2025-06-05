04ec7cf75682725894ff042d4df58da42502d1b8
Gealor <gzazulya@gmail.com>
2025-06-05 22:45:44 +0400

    connect auth api router to the main app router

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from .api_v1 import router as router_api_v1
+from .auth_api import router as router_auth_api
 from core.config import settings
 
 router = APIRouter(
@@ -9,5 +10,9 @@ router = APIRouter(
 
 router.include_router(
     router_api_v1,
+)
+
+router.include_router(
+    router_auth_api,
 )
 
