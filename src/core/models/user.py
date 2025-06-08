874e5d28672f7076b45b80d9cc590ac416e22fed
Gealor <gzazulya@gmail.com>
2025-06-08 21:22:26 +0400

    little changes in users table

--- src/core/models/user.py
+++ src/core/models/user.py
@@ -1,6 +1,5 @@
 from typing import Optional, TYPE_CHECKING  # флаг для избежания циклической зависимости
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import DateTime, UniqueConstraint
 
 from .mixins.created_at_mixin import CreatedAtMixin
 from .mixins.int_id_pk import IntIdPkMixin
@@ -16,7 +15,7 @@ class User(IntIdPkMixin, CreatedAtMixin, Base):
     username : Mapped[str] = mapped_column(unique = True)
     mail : Mapped[str] = mapped_column(unique = True)
                                                # server_default для заполнения этого столбца всех строк указанным значением
-    password : Mapped[str] = mapped_column(default = True, server_default="password_test", nullable=False)
+    password : Mapped[str] = mapped_column(nullable=False)
 # back_populates указывает на то, с какого поля в другой модели будет происходить обратная связь(т.е. с какого поля в другой модели будет происходить связь с этой моделью)
     info_user : Mapped[Optional["InfoUser"]] = relationship(back_populates="user", cascade="all, delete-orphan")
 # cascade = "all, delete" нужно, чтобы при удалении записи из users удалялись связанные с этой записью строки из связанной таблицы
