a4f04e436375da116b6a64e148c40a2fef86dd64
Gealor <gzazulya@gmail.com>
2025-08-21 22:58:37 +0400

    fix create_at mixin

--- src/core/models/mixins/created_at_mixin.py
+++ src/core/models/mixins/created_at_mixin.py
@@ -1,8 +1,11 @@
 
-from datetime import datetime
+from datetime import datetime, timezone
 from sqlalchemy import TIMESTAMP, DateTime, func
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped, mapped_column
 
 class CreatedAtMixin:
-    created_at : Mapped[datetime] = mapped_column(default = datetime.now(), server_default="now()", nullable = False) 
+    created_at : Mapped[datetime] = mapped_column(
+        default = lambda : datetime.now(tz=timezone.utc).replace(tzinfo=None), 
+        server_default=func.now(), 
+        nullable = False) 
